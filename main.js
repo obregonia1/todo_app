faeb9e5f60c412e4b54965de4ac04326d5f0bdce
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-05-05 11:35:46 +0900

    Todo追加機能を実装

---
+++ main.js
@@ -0,0 +1,37 @@
+const app = {
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+      editedTodo: '',
+      edit: ''
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('todos')) {
+      try {
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+      } catch (e) {
+        localStorage.removeItem('todos');
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) {
+        return;
+      }
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+      this.saveTodos();
+    },
+    saveTodos() {
+      let parsed = JSON.stringify(this.todos);
+      localStorage.setItem('todos', parsed);
+    }
+  }
+}
+
+Vue.createApp(app).mount('#app');
+
+
